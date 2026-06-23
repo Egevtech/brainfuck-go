5058eadbfa57c407ebf4b9bb594a73659d2192b5
Egevtech <egevtech@gmail.com>
2026-06-23 20:45:50 +0300

    Add full loops support

--- codegen.go
+++ codegen.go
@@ -5,11 +5,13 @@ import "fmt"
 func Codegen(tokens []Token) string {
 	var contents string
 
-	stdfunctions := "vector_init, next_cell, prev_cell, add_cell, sub_cell, print_cell, print_cell_num"
+	stdfunctions := "vector_init, read_cell, next_cell, prev_cell, add_cell, sub_cell, print_cell, print_cell_num"
 
 	contents += "section .data\nglobal _start\n\nvec dq 0\ncell dd 0\n\nextern "
 	contents += stdfunctions
 	contents += "\n\nsection .text\n_start:\n\tcall vector_init\n\tmov [vec], rax\n\n"
+
+	nesting_level := 0
 
 	ForEach(tokens, func(index int, token Token) {
 		var stdcall string
@@ -27,6 +29,17 @@ func Codegen(tokens []Token) string {
 			stdcall = "print_cell"
 		case TOKEN_DEBUG:
 			stdcall = "print_cell_num"
+
+		case TOKEN_LOOP_START:
+			nesting_level++
+			contents += fmt.Sprintf("\n;----LOOP%d-START----\n\tloop%d:\n", nesting_level, nesting_level)
+			contents += "mov rdi, [vec]\n\tmov rsi, cell\n"
+			contents += fmt.Sprintf("\tcall read_cell\n\tcmp rax, 0\n\tje loop%d_end\n\n", nesting_level)
+			return
+		case TOKEN_LOOP_END:
+			contents += fmt.Sprintf("\tjmp loop%d\n\tloop%d_end:\n;----LOOP%d-END----\n\n", nesting_level, nesting_level, nesting_level)
+			nesting_level--
+			return
 		}
 
 		contents += fmt.Sprintf("\tmov rdi, [vec]\n\tmov rsi, cell\n\tcall %s\n\n", stdcall)
